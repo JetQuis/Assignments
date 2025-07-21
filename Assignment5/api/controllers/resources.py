80b2a91e130dbd0bfcb8f10884c4954ba044db08
Quis <mfowler1616@gmaol.com>
2025-07-21 17:45:58 -0400

    Create operation

--- Assignment5/api/controllers/resources.py
+++ Assignment5/api/controllers/resources.py
@@ -1,1 +1,16 @@
 # Resources controller
+
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import models, schemas
+
+def create(db: Session, resource: schemas.ResourceCreate):
+    db_resource = models.Resource(
+        name=resource.name,
+        quantity=resource.quantity
+    )
+    db.add(db_resource)
+    db.commit()
+    db.refresh(db_resource)
+    return db_resource
+
