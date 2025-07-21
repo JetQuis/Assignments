c63999de1919a839998790a6622cdffe687e5dac
Quis <mfowler1616@gmaol.com>
2025-07-21 18:16:18 -0400

    completed operations

--- Assignment5/api/controllers/recipes.py
+++ Assignment5/api/controllers/recipes.py
@@ -14,3 +14,27 @@ def create(db: Session, recipe: schemas.RecipeCreate):
     db.commit()
     db.refresh(db_recipe)
     return db_recipe
+
+def read_all(db: Session):
+    return db.query(models.Recipe).all()
+
+
+def read_one(db: Session, recipe_id: int):
+    return db.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
+
+def update(db: Session, recipe_id: int, recipe: schemas.RecipeUpdate):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    update_data = recipe.model_dump(exclude_unset=True)
+    db_recipe.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_recipe.first()
+
+
+def delete(db: Session, recipe_id: int):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    if db_recipe.first() is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+
+    db_recipe.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
