29d026f1e268f0a5c6de96bee452674c0249c6dc
Quis <mfowler1616@gmaol.com>
2025-07-21 15:21:05 -0400

    create operation

--- Assignment5/api/controllers/sandwiches.py
+++ Assignment5/api/controllers/sandwiches.py
@@ -1,1 +1,16 @@
-#Sandwiches controller
+# Sandwiches controller
+
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import models, schemas
+
+def create(db: Session, sandwich: schemas.SandwichCreate):
+    db_sandwich = models.Sandwich(
+        name=sandwich.name,
+        size=sandwich.size,
+        cost=sandwich.cost
+    )
+    db.add(db_sandwich)
+    db.commit()
+    db.refresh(db_sandwich)
+    return db_sandwich
